659844c62f9fc325e6591f659a84310b520e991f
kalaiyarasan <kalaiyarasan.ayyappan@chainsys.com>
2022-06-08 13:17:54 +0530

    abstract demo

--- src/com/chainsys/bestPractices/AbstractDemo/ScoreBoard.java
+++ src/com/chainsys/bestPractices/AbstractDemo/ScoreBoard.java
@@ -2,9 +2,12 @@ package com.chainsys.bestPractices.AbstractDemo;
 public class ScoreBoard
 {
 public int TargetScore=215; 
-private ScoreBoard(){
+private ScoreBoard()
+{
 System.out.println("Object created for ScoreBoard");
 } //createppject() is a factory method
-public static ScoreBoard createObject() { return new ScoreBoard();
+public static ScoreBoard createObject() 
+{
+	return new ScoreBoard();
 }
 }
